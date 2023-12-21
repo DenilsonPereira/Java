f2b192c6dc9820dfdb7c500479455a3df476d531
Denilson <denilsongunner@gmail.com>
2023-12-20 22:01:58 -0300

    Exercicio sobre enum

---
+++ AulaEnum/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program {
+	public static void main(String[] args) {
+		Order order = new Order(10, new Date(), OrderStatus.PANDING_PAYMENTS);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		System.out.println(os1);
+		System.out.println(os2);
+	}
+}
---
+++ AulaEnum/src/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum OrderStatus {
+	PANDING_PAYMENTS,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
